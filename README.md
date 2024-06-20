d3a17ca620da7246d40b2914bb8dba9ae1b72b7d
Orion Railean <orion.railean@gmail.com>
2024-06-20 13:18:49 +1000

    add more graph stuff (unfinished)

---
+++ README.md
@@ -0,0 +1,7 @@
+# 3D Geometry Dash Graphs
+
+This is mainly an analysis of perspective distortion when showing a 3D image on a 2D screen. I'll use this to assist in creating a 3D Geometry Dash level.
+
+## Usage
+
+Render ring.blend in Blender (It's in 32K resolution, so it might take a while). Then, create a directory called `input` and put the rendered image in there. Then, run `analyze.py`. A few graphs should open.
--- analyze.py
+++ analyze.py
@@ -4,8 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def ease_in_custom(t):
-    return np.power(t, 1.85)
+def ease_in_custom(npArray):
+    return np.power(npArray, 1.85)
+
+
+def linear_easing(npArray):
+    return np.divide(npArray, 1.85)
 
 
 input_folder = 'input'
@@ -23,13 +27,13 @@ for contour in contours:
     cY = int(M["m01"] / M["m00"])
     centers.append((cX, cY))
 
-centers.sort(key=lambda x: x[1])
+centers.sort(key=lambda _: _[1])
 
-rows: list[list[list[int]]] = []
-columns: dict[int, list] = {}
-current_row: list[list[int]] = []
+current_row: list[tuple[int, int]] = []
+rows: list[list[tuple[int, int]]] = []
+columns: dict[int, list[tuple[int, int]]] = {}
 prev_y = centers[0][1]
-threshold = 10
+threshold = 100
 
 for center in centers:
     if abs(center[1] - prev_y) > threshold:
@@ -41,38 +45,41 @@ for center in centers:
         columns[center[0]].append(center)
     current_row.append(center)
     prev_y = center[1]
-    current_row.sort(key=lambda x: x[0])
-    columns[center[0]].sort(key=lambda x: x[1])
+    current_row.sort(key=lambda _: _[0])
+    columns[center[0]].sort(key=lambda _: _[1])
 
 rows.append(current_row)
+columns = dict(sorted(columns.items()))
 
 new_image = np.zeros_like(image)
 
-x_dist = []
+row_x_y_values = []
+left_column = columns[list(columns.keys())[0]]
 
 e = 0
 for row in rows:
+    colour = [0, 0, 255]
     if e == 0:
-        colour = (0, 255, 0)
-    else:
-        colour = (0, 0, 255)
+        colour = [0, 255, 0]
     for i in range(len(row)):
-        cv2.circle(new_image, row[i], 5, colour, -1)
+        if row[i] in left_column:
+            newColour = [255, 0, 0]
+        else:
+            newColour = colour
+        cv2.circle(new_image, row[i], 5, newColour, -1)
 
         if i < len(row) - 1:
-            cv2.line(new_image, row[i], row[i + 1], colour, 2)
+            cv2.line(new_image, row[i], row[i + 1], newColour, 2)
             if e == 0:
-                x_dist.append(row[i + 1][0] - row[i][0])
+                row_x_y_values.append(row[i + 1][0] - row[i][0])
     e += 1
 
 cv2.imwrite('output.png', new_image)
 
 top_row = rows[0]
-left_column = columns[list(columns.keys())[0]]
 
-row_x_values = [i + round(len(top_row) / 2) for i in range(round(len(top_row) / 2))]
+row_x_values = [i + len(top_row) // 2 for i in range(len(top_row) // 2)]
 row_y_y_values = [center[1] for center in top_row]
-row_x_y_values = x_dist
 
 row_y_big = min(row_y_y_values)
 row_y_small = max(row_y_y_values)
@@ -86,16 +93,43 @@ i = 0
 for x, y in top_row:
     if i >= len(top_row) / 2:
         row_y_graph.append((y - row_y_small) / (row_y_big - row_y_small))
-        row_x_graph.append((x_dist[i - 1] - row_x_small) / (row_x_big - row_x_small))
+        row_x_graph.append((row_x_y_values[i - 1] - row_x_small) / (row_x_big - row_x_small))
     i += 1
 
-t = np.linspace(0, 1, round(len(top_row) / 2))
+t = np.linspace(0, 1, len(top_row) // 2)
 y_custom = ease_in_custom(t)
 
 plt.plot(row_x_values, y_custom, label='Simulated Easing')
 plt.plot(row_x_values, row_y_graph, label='Top Row Y Dif (Render)')
 plt.plot(row_x_values, row_x_graph, label='Top Row X Dif (Render)')
 plt.title('Top Row X/Y Difference')
+plt.xlabel('Dot Number')
+plt.ylabel('Difference')
+plt.grid(True)
+plt.legend()
+plt.show()
+
+column_x_values = [i + len(left_column) // 2 for i in range(len(left_column) // 2)]
+column_x_x_values = [center[0] for center in left_column]
+column_y_values = []
+
+for i in range(len(left_column)):
+    column_y_values.append(left_column[i][1] - rows[i][len(rows[i]) // 2][1])
+
+column_y_big = max(column_y_values[len(left_column) // 2:])
+column_y_small = min(column_y_values[len(left_column) // 2:])
+
+column_y_graph = []
+
+for i in range(len(left_column) // 2):
+    column_y_graph.append((column_y_values[i + len(left_column) // 2] - column_y_small) / (column_y_big - column_y_small))
+
+t = np.linspace(0, 1, len(left_column) // 2)
+y_custom = linear_easing(t)
+
+plt.plot(column_x_values, y_custom, label='Simulated Easing')
+plt.plot(column_x_values, column_y_graph, label='Left Column Y Dif (Render)')
+plt.title('Left Column Y Difference')
 plt.xlabel('Dot Number')
 plt.ylabel('Difference')
 plt.grid(True)
